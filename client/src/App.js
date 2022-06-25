cfb3622ba7738beaf6abe427407617638cb01255
yusuf963 <mohammad963yusuf@gmail.com>
2022-06-25 23:35:37 +0100

    render categories on app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,36 @@
 const App =()=> {
+  const categories=[
+    {
+      id:1,
+      title: 'Hats'
+    },
+    {
+      id:2,
+      title: 'Jackets'
+    },    {
+      id:3,
+      title: 'Sneakers'
+    },    {
+      id:4,
+      title: 'Womens'
+    },    {
+      id:5,
+      title: 'Mens'
+    },
+  ]
+
   return (
-    <div className="App">
+    <div className="categories-container">
+      {categories.map(({id, title})=>{
+        return(
+            <div className="category-container" key={id}>
+              <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Show More</p>
+              </div>
+            </div>
+            )
+      })}
     </div>
   );
 }
