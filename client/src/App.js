278f1506ea34433861512d8ac486c49e578b2811
yusuf963 <mohammad963yusuf@gmail.com>
2022-06-26 11:35:43 +0100

    install firebase and create dummy sgign-in component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import { Navigation } from './components/navigation/Navigation'
 import {Home} from './pages/Home'
+import {SignIn} from './pages/SignIn'
 
 const Shop =()=>{
   return(
@@ -15,6 +16,7 @@ const App =()=> {
       <Route path='/' element={<Navigation/>}>
       <Route index element={<Home/>}/>
       <Route path='/shop' element={<Shop/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
       </Route>
     </Routes>
   );
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+const SignIn = () =>{
+    return(
+        <div>
+            <p>Sign in page</p>
+        </div>
+    )
+}
+
+export {SignIn}
