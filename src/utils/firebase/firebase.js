4c67782ae1d3d850e1c620881052524f0a1a28e2
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-02 20:57:19 +0100

    add travis.yml config

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -15,9 +15,9 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
-  console.log(firebaseConfig.apiKey,firebaseConfig.authDomain, firebaseConfig.databaseURL,firebaseConfig.projectId,
-    firebaseConfig.storageBucket,firebaseConfig.messagingSenderId, firebaseConfig.appId,firebaseConfig.measurementId
-    )
+  // console.log(firebaseConfig.apiKey,firebaseConfig.authDomain, firebaseConfig.databaseURL,firebaseConfig.projectId,
+  //   firebaseConfig.storageBucket,firebaseConfig.messagingSenderId, firebaseConfig.appId,firebaseConfig.measurementId
+  //   )
   
   // Initialize Firebase
   initializeApp(firebaseConfig);
